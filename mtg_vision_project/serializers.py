b7af8bb431063e4ea832224e58652480309dd342
temescalMike <michael.surdouski@temescalwellness.com>
2020-12-14 12:17:51 -0500

    added try except block around entire api view

--- mtg_vision_project/serializers.py
+++ mtg_vision_project/serializers.py
@@ -1,3 +1,5 @@
+import logging
+
 import cv2
 import pandas as pd
 import numpy as np
@@ -30,28 +32,32 @@ class OutPutSerializer(serializers.ModelSerializer):
 @login_required()
 @api_view(['POST'])
 def upload_api_view(request):
-    serializer = UploadImageSerializer(data=request.data)
-    is_valid = serializer.is_valid()
-    if is_valid:
-        image_upload_object = serializer.save()
-        #image = Image.open(serializer.validated_data['image_input'])
-        image = cv2.imread(f'{MEDIA_ROOT}{image_upload_object.image_input}')
-        del image_upload_object
-        hash_pool = pd.read_pickle(CARD_POOLS_ALL_SETS_PCK)
-        hash_pool = flatten_hash_array(hash_pool)
-        detected_cards = find_cards(image, hash_pool)
-        del hash_pool
-        del serializer
-        del image
-        card_models = [
-            ImageUpload.objects.create(image_input=path.image_path,
-                                       image_name=path.card_name)
-            for path in detected_cards
-        ]
-        del detected_cards
-        card_serializers = OutPutSerializer(card_models, many=True)
-        return Response({"message": "Success.", "data": card_serializers.data})
-    return Response({"message": "Bad input type.", "data": serializer.data}, status=400)
+    try:
+        serializer = UploadImageSerializer(data=request.data)
+        is_valid = serializer.is_valid()
+        if is_valid:
+            image_upload_object = serializer.save()
+            #image = Image.open(serializer.validated_data['image_input'])
+            image = cv2.imread(f'{MEDIA_ROOT}{image_upload_object.image_input}')
+            del image_upload_object
+            hash_pool = pd.read_pickle(CARD_POOLS_ALL_SETS_PCK)
+            hash_pool = flatten_hash_array(hash_pool)
+            detected_cards = find_cards(image, hash_pool)
+            del hash_pool
+            del serializer
+            del image
+            card_models = [
+                ImageUpload.objects.create(image_input=path.image_path,
+                                           image_name=path.card_name)
+                for path in detected_cards
+            ]
+            del detected_cards
+            card_serializers = OutPutSerializer(card_models, many=True)
+            return Response({"message": "Success.", "data": card_serializers.data})
+    except Exception as e:
+        logging.critical('failed, shut down everything', e.with_traceback())
+        exit(1)
+    return Response({"message": "Bad input type.", "data": "failed"}, status=400)
 
 
 def flatten_hash_array(card_pool):
