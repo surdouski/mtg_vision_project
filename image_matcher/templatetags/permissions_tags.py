98f349be316ed50aa0b9ac01562a83c6ece8c9a3
temescalMike <michael.surdouski@temescalwellness.com>
2020-12-26 11:18:33 -0500

    Updated with permissions on templates. Also should now properly set up authentication.

---
+++ image_matcher/templatetags/permissions_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='has_ebay_tokens')
+def has_ebay_tokens(user):
+    """Returns true if user already has already signed up through ebay."""
+    return True if user.user_tokens_profile.refresh_token else False
